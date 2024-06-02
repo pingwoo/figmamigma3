1a1f3e59109aaeae187ac12180e0a6be3cd3e057
pingwoo <164857632+pingwoo@users.noreply.github.com>
2024-06-02 17:54:48 +0300

    Delete вет directory

--- вет/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".navigation_hamburger");
-const forhamburger = document.querySelector(".forhamburger");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    forhamburger.classList.toggle("active");
-    }
-)
